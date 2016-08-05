17b9d138e0421cdffd8aa23b50cde3f4def1f287
Chetan Kothari <mail@chetankothari.in>
2016-08-05 17:16:53 +0530

    Implement valid-user

--- exercises/user-auth/src/user_auth/core.clj
+++ exercises/user-auth/src/user_auth/core.clj
@@ -2,4 +2,8 @@
 
 (defn valid-user?
   [user_tokens request_headers]
-  false)
+  (let [
+      user-name (get request_headers "User-Name")
+      authorisation-str (get request_headers "Authorisation" "")
+      [algo-type user-token] (clojure.string/split authorisation-str #" ")]
+    (and (= user-token (get user_tokens user-name)) (= algo-type "Bearer"))))
